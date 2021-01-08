9bb255ef37247175be063b91937a44343ceb593f
ahmedamged <ahmedamged@live.com>
2021-01-08 23:56:10 +0200

    Made http requests to fake Rest API with axios and handling responses - used vue router

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,21 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <!-- <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div> -->
   <Header />
-  <AddTodo v-on:add-todo="addTodo"/>
-  <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+  <router-view/>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import Todos from './components/Todos.vue';
 import Header from './components/layout/Header.vue';
-import AddTodo from './components/AddTodo.vue';
 
-export default {
-  name: 'App',
-  components: {
-    Todos,
-    Header,
-    AddTodo
-  },
-  data() {
-    return{
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: false
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        }
-      ]
-    }
-  },
-  methods: {
-    deleteTodo(id){
-      this.todos = this.todos.filter(todo => todo.id !== id);
-    },
-    addTodo(newTodo){
-      this.todos = [...this.todos, newTodo];
+  export default{
+    name: "app",
+    components: {
+      Header
     }
   }
-}
 </script>
 
 <style>
@@ -58,26 +25,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 } */
-  *{
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-  body{
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
-  .btn{
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
-  }
-  .btn:hover{
-    background: #666;
-  }
+
+/* #nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+} */
 </style>
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <header class="header">
     <h1>TodoList</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
   </header>
 </template>
 
@@ -20,5 +24,19 @@
   .header a{
     color: #fff;
     padding-right: 5px;
+  }
+
+  #nav {
+    padding: 30px;
+  }
+
+  #nav a {
+    font-weight: bold;
+    color: #b9eddc;
+    text-decoration: none;
+  }
+
+  #nav a.router-link-exact-active {
+    color: #42b983;
   }
 </style>
