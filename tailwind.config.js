ca3ce5d5897871b2dff1814c1c94bda19b8980f1
ahmedamged <ahmedamged@live.com>
2021-01-11 23:25:19 +0200

    Adding tailwindcss and postcss7 - editing configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+// const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [
+    './public/**/*.html',
+    './src/**/*.vue',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    // fontFamily: {
+    //   sans: ['Inter', ...defaultTheme.fontFamily.sans],
+    // },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
